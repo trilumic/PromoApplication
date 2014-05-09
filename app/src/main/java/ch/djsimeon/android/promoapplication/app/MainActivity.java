644c67720396229bb654febe418a1226a6052e93
Thoemsey <thoemsey@gmail.com>
2014-05-09 09:29:43 +0200

    Hallöchen

--- app/src/main/java/ch/djsimeon/android/promoapplication/app/MainActivity.java
+++ app/src/main/java/ch/djsimeon/android/promoapplication/app/MainActivity.java
@@ -7,6 +7,8 @@ import android.view.MenuItem;
 
 public class MainActivity extends Activity {
 
+    //Hallo teammember
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
