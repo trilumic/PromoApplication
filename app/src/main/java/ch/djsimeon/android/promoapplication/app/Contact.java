bf86d6282682f864d332f0618f49a10ca6dd87d5
trilumic <trilumic@gmail.com>
2014-05-09 10:28:50 +0200

    Menü erstellt,
    onCreateOptionsMenu() angepasst (für jede Activity übernehmen!),
    onOptionsItemSelected angepasst (in jeder Activity übernehmen!),
    
    XML-Files für Menü erstellt (main.xml)

--- app/src/main/java/ch/djsimeon/android/promoapplication/app/Contact.java
+++ app/src/main/java/ch/djsimeon/android/promoapplication/app/Contact.java
@@ -1,6 +1,7 @@
 package ch.djsimeon.android.promoapplication.app;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -18,7 +19,7 @@ public class Contact extends Activity {
     public boolean onCreateOptionsMenu(Menu menu) {
         
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.contact, menu);
+        getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
@@ -28,10 +29,22 @@ public class Contact extends Activity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+
+        switch (id){
+            case R.id.action_contact:
+                Intent contact = new Intent(this,Contact.class);
+                startActivity(contact);
+//            case  R.id.action_tourdates:
+//                Intent tour = new Intent(this,Tourdates.class);
+//                startActivity(tour);
+//            case R.id.action_music:
+//                Intent music = new Intent(this, Music.class);
+//                startActivity(music);
+//            case R.id.action_gallery:
+//                Intent gallery = new Intent(this, Gallery.class);
+//                startActivity(gallery);
+
         }
         return super.onOptionsItemSelected(item);
     }
-
 }
--- app/src/main/java/ch/djsimeon/android/promoapplication/app/MainActivity.java
+++ app/src/main/java/ch/djsimeon/android/promoapplication/app/MainActivity.java
@@ -1,13 +1,12 @@
 package ch.djsimeon.android.promoapplication.app;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends Activity {
-
-    //Hallo teammember
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,15 +14,10 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
     }
 
-    //marc macht nüt
-
-
-    //Dies ist ein Testkommentar
-    //Noch ein Kommentar!!!!!!!!
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        
+
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
@@ -35,9 +29,25 @@ public class MainActivity extends Activity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+
+        switch (id){
+            case R.id.action_contact:
+                Intent contact = new Intent(this,Contact.class);
+                startActivity(contact);
+//            case  R.id.action_tourdates:
+//                Intent tour = new Intent(this,Tourdates.class);
+//                startActivity(tour);
+//            case R.id.action_music:
+//                Intent music = new Intent(this, Music.class);
+//                startActivity(music);
+//            case R.id.action_gallery:
+//                Intent gallery = new Intent(this, Gallery.class);
+//                startActivity(gallery);
+
         }
+//        if (id == R.id.action_contact) {
+//            return true;
+//        }
         return super.onOptionsItemSelected(item);
     }
 
