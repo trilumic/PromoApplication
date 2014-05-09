f30ee79d2490f87092bd0e55067bf8a64a572980
Marc B <marc.brueni@hispeed.ch>
2014-05-09 11:05:24 +0200

    Tourdates menü

--- app/src/main/java/ch/djsimeon/android/promoapplication/app/Tourdates.java
+++ app/src/main/java/ch/djsimeon/android/promoapplication/app/Tourdates.java
@@ -1,6 +1,7 @@
 package ch.djsimeon.android.promoapplication.app;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -11,6 +12,7 @@ public class Tourdates extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tourdates);
+        //asdf
     }
 
 
@@ -28,8 +30,20 @@ public class Tourdates extends Activity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+
+        switch (id){
+            case R.id.action_contact:
+                Intent contact = new Intent(this,Contact.class);
+                startActivity(contact);
+//            case  R.id.action_tourdates:
+//                Intent tour = new Intent(this,Tourdates.class);
+//                startActivity(tour);
+//            case R.id.action_music:
+//                Intent music = new Intent(this, Music.class);
+//                startActivity(music);
+//            case R.id.action_gallery:
+//                Intent gallery = new Intent(this, Gallery.class);
+//                startActivity(gallery);
         }
         return super.onOptionsItemSelected(item);
     }
