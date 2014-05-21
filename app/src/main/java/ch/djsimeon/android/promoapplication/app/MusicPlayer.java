5a89d8c46d2b645bc88d926f9041fd9672e46159
Marc B <marc.brueni@hispeed.ch>
2014-05-21 14:45:35 +0200

    musicplayer outofmemory bitmap downscaling

--- app/src/main/java/ch/djsimeon/android/promoapplication/app/MusicPlayer.java
+++ app/src/main/java/ch/djsimeon/android/promoapplication/app/MusicPlayer.java
@@ -5,6 +5,9 @@ import java.text.DecimalFormat;
 import java.util.concurrent.TimeUnit;
 
 import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
@@ -40,6 +43,44 @@ public class MusicPlayer extends Activity {
 //        return output;
 //
 //    }
+    public static int calculateInSampleSize(
+            BitmapFactory.Options options, int reqWidth, int reqHeight) {
+        // Raw height and width of image
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+
+        if (height > reqHeight || width > reqWidth) {
+
+            final int halfHeight = height / 2;
+            final int halfWidth = width / 2;
+
+            // Calculate the largest inSampleSize value that is a power of 2 and keeps both
+            // height and width larger than the requested height and width.
+            while ((halfHeight / inSampleSize) > reqHeight
+                    && (halfWidth / inSampleSize) > reqWidth) {
+                inSampleSize *= 2;
+            }
+        }
+
+        return inSampleSize;
+    }
+
+    public static Bitmap decodeSampledBitmapFromResource(Resources res, int resId,
+                                                         int reqWidth, int reqHeight) {
+
+        // First decode with inJustDecodeBounds=true to check dimensions
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeResource(res, resId, options);
+
+        // Calculate inSampleSize
+        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
+
+        // Decode bitmap with inSampleSize set
+        options.inJustDecodeBounds = false;
+        return BitmapFactory.decodeResource(res, resId, options);
+    }
 
     boolean seeking = false;
     @Override
@@ -129,7 +170,10 @@ public class MusicPlayer extends Activity {
         startTime = 0;
         seekbar.setMax((int) finalTime);
         ImageView cover = (ImageView) findViewById(R.id.cover_image);
-        cover.setImageResource(musicPieceCovers[index]);
+        int size = Math.min(cover.getWidth(), cover.getHeight());
+        cover.setImageBitmap(
+                decodeSampledBitmapFromResource(getResources(), musicPieceCovers[index], size, size));
+//        cover.setImageResource(musicPieceCovers[index]);
         endTimeField.setText(String.format("%02d : %02d",
                 TimeUnit.MILLISECONDS.toMinutes((long) finalTime),
                 TimeUnit.MILLISECONDS.toSeconds((long) finalTime) -
